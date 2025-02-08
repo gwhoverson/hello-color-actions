ba4f283ad73a948515f81cf502ba76f56b909908
gwhoverson <gregoryhoverson@att.net>
2025-02-08 09:15:53 -0600

    Update the hello message

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from a novice in Nashville";
 console.log(hello());
 exports.hello = hello;
